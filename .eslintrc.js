fe22c5a2423fbeb587599a76c08ccc34423ee58d
yauzz <yauzz.scu@gmail.com>
2016-12-23 14:42:17 +0800

    增加ESLint 语法提示

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module",
+        "ecmaVersion": "6"
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": "off",
+        "no-undef": "off",
+        "no-unused-vars": "warn"
+    }
+};
